9db6bb89ebd8228c872e9f685164b2e2854d61bc
Beatriz Sara <bea.sara.santos@outlook.com>
2026-02-05 23:33:53 -0300

    alteracao codigo js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log("Hello, Star Wars!");
+
+var userName = "Fernando Papito"
+
+document.getElementById('user-name').innerHTML = userName;
+
+// var nome = "Mestre Yoda";
+// var idade = 100;
+// var jedi = true;
+
+// console.log(typeof nome);
+// console.log(typeof idade);
+// console.log(typeof jedi);
