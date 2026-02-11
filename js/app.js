0b9e55dd418647c814d15bc249fdd61274106df7
Beatriz Sara <bea.sara.santos@outlook.com>
2026-02-11 12:05:48 -0300

    1.2 operadores matematicos

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ var subtracao = n1 - n2
 var multiplicacao = n1 * n2
 var divisao = n1 / n2
 
-console.log(soma)
-console.log(subtracao)
-console.log(multiplicacao)
-console.log(divisao)
+console.log(typeof soma)
+console.log(typeof subtracao)
+console.log(typeof multiplicacao)
+console.log(typeof divisao)
