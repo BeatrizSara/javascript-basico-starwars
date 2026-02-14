ace1cb38bad9909329e74ca2f4adecfef6f1da72
Beatriz Sara <bea.sara.santos@outlook.com>
2026-02-13 21:34:47 -0300

    cometarios de revisao do curso JS para QAs

--- js/app.js
+++ js/app.js
@@ -6,16 +6,19 @@ document.getElementById('user-name').innerHTML = userName;
 
 // variáveis e tipos de dados
 
-// var nome = "Mestre Yoda";
-// var idade = 100;
-// var jedi = true;
+/*
+var nome = "Mestre Yoda";
+var idade = 100;
+var jedi = true;
 
-// console.log(typeof nome);
-// console.log(typeof idade);
-// console.log(typeof jedi);
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof jedi);
+*/
 
 // Operadores Matemáticos
 
+/* 
 var n1 = 10
 var n2 = 5
 
@@ -24,7 +27,34 @@ var subtracao = n1 - n2
 var multiplicacao = n1 * n2
 var divisao = n1 / n2
 
++ para somar
+- para subtrair
+* para multiplicar
+/ para dividir
+
 console.log( soma)
 console.log(typeof subtracao)
 console.log(typeof multiplicacao)
 console.log(typeof divisao)
+*/
+
+// Operadores de comparação
+
+/* 
+var n1 = 5
+var n2 = 5
+
+var igual = n1 == n2
+var diferente = n1 != n2
+var maior = n1 > n2
+var menor = n1 < n2
+var maiorIgual = n1 >= n2
+var menorIgual = n1 <= n2
+
+console.log(igual)
+console.log(diferente)
+console.log(maior)
+console.log(menor)
+console.log(maiorIgual)
+console.log(menorIgual)
+*/
