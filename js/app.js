01d02a5022bcc6b2f3486cc8c9c67929b9fe5a28
Beatriz Sara <bea.sara.santos@outlook.com>
2026-02-13 22:54:14 -0300

    controle de fluxo - bdd

--- js/app.js
+++ js/app.js
@@ -87,3 +87,38 @@ function soma(a, b) {
 var resultado = soma(5, 10)
 console.log(resultado)
 */
+
+// Controle de fluxo
+
+/* 
+
+-> Escrevendo o comportamento antes do software pronto - BDD
+
+Sendo um cliente corretista do banco
+Posso sacar dinheiro do caixa eletrônico
+Para poder comprar em lugares que não aceitam cartão de débido ou crédito
+
+Cenário 1: Saque com sucesso
+Dado que meu saldo é de R$ 1000,00
+Quando faço um saque de R$ 500,00
+Entao o valor do meu saque deve ser deduzido do meu saldo
+
+
+
+Cenário 2: Saque com valor superior ao saldo
+Dado que meu saldo é de R$ 1000,00
+Quando faço um saque de R$ 1001,00
+Então não deve deduzir o valor do meu saldo
+E deve mostrar uma mensagem de alerta informando que o valor do saque é maior do que o saldo disponível
+
+
+
+Cenário 3: Saque com valor máximo
+Dado que meu saldo é de R$ 1000,00
+E o valor maximo por por operacao é de R$ 700,00 (PO informou para o QA o valor maximo por operacao)
+Quando faço um saque de R$ 701,00
+Então não deve deduzir o valor do meu saldo
+E deve mostrar uma mensagem de alerta informando que o valor do saque é maior do que o valor máximo permitido por operação
+
+
+*/ 
