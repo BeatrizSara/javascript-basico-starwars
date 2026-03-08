f9a00039e163ba36591e362e583b1cee8b29f009
Beatriz Sara <bea.sara.santos@outlook.com>
2026-03-08 01:11:44 -0300

    add const LIST with 8 items

--- js/app.js
+++ js/app.js
@@ -1,7 +1,55 @@
+// T    em um array de objetos com 8 itens.
+const LIST = [
+    {
+        id: 1,
+        name: 'Yoda',
+        avatar: 'images/yoda.png'
+    },
+    {
+        id: 2,
+        name: 'Luke Skywalker',
+        avatar: 'images/luke.png'
+    },
+
+    {
+        id: 3,
+        name: 'Princess Leia',
+        avatar: 'images/leia.png'
+    },
+    {
+        id: 4,
+        name: 'Han Solo',
+        avatar: 'images/hansolo.png'
+    },
+
+    {
+        id: 5,
+        name: 'Darth Vader',
+        avatar: 'images/vader.png'
+    },
+    
+    {
+        id: 6,
+        name: 'Chewbacca',
+        avatar: 'images/chewbacca.png'
+    },
+    {
+        id: 7,
+        name: 'R2D2',
+        avatar: 'images/r2d2.png'
+    },
+    {
+        id: 8,
+        name: 'C3PO',
+        avatar: 'images/c3po.png'
+    }
+];
+
 const App = new Vue({
     el: '#app', // ele vai entender que essa div é a principal da aplicação.
-    title: 'Star Wars',
+    title: 'Star Wars Lego',
     userName: 'Beatriz',
+    characters: LIST
 }) 
 
 //exemplo de uso do Vue.js
